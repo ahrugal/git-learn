e343bf184707fa42ad016147097fb7c46460884a
ahrugal <mr.cmelin@gmail.com>
2018-04-29 23:38:43 +0200

    Python test

--- start.py
+++ start.py
@@ -1,12 +1,15 @@
-"Test GIT"
-yrdy
+number = int(input("Give me a number ---> :").strip())
+check = int(input("Give me another number --> :").strip())
 
-yrdy
+if number % 4 == 0:
+    print("Number is even and divisible by four")
+elif number % 2 == 0:
+    print("Number is even")
+elif number % 2 != 0:
+    print("Number is odd")
 
-
-# TEMP: # REVIEW:
-{
-y
-
-
-: value for key, value in variable}
+print("Exercise 2")
+if number % check == 0:
+    print("Division turned even")
+elif number % check != 0:
+    print("Division turned odd")
